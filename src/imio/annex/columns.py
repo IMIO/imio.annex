a6b017ddf2a5c9656f4ede98266c495464a88332
Gauthier Bastien <g.bastien@imio.be>
2016-09-29 16:53:59 +0200

    Download the file when clicking on it's title

--- src/imio/annex/columns.py
+++ src/imio/annex/columns.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-from collective.iconifiedcategory.interfaces import IIconifiedPreview
 from imio.dashboard.columns import ActionsColumn as DashboardActionsColumn
 from imio.dashboard.columns import PrettyLinkColumn as DashboardPrettyLinkColumn
 
@@ -13,9 +12,3 @@ class ActionsColumn(DashboardActionsColumn):
 class PrettyLinkColumn(DashboardPrettyLinkColumn):
     header = u'Title'
     weight = 20
-
-    def renderCell(self, obj):
-        if IIconifiedPreview(obj.getObject()).converted is True:
-            self.params['target'] = '_blank'
-            self.params['appendToUrl'] = '/documentviewer#document/p1'
-        return super(PrettyLinkColumn, self).renderCell(obj)
