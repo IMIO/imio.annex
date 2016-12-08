c03fc0b6fc33a691754d13d14808563baf0639ae
Gauthier Bastien <g.bastien@imio.be>
2016-12-08 12:16:25 +0100

    Back to development: 1.2

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ long_description = (
 
 setup(
     name='imio.annex',
-    version='1.1',
+    version='1.2.dev0',
     description="An add-on for Plone",
     long_description=long_description,
     # Get more from https://pypi.python.org/pypi?%3Aaction=list_classifiers
