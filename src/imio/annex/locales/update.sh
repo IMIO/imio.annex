bf406542cd05aded7ab5eb4925f9c61edde5edfd
Martin Peeters <martin.peeters@affinitic.be>
2016-12-21 17:36:12 +0100

    Improve update locales script

--- src/imio/annex/locales/update.sh
+++ src/imio/annex/locales/update.sh
@@ -7,9 +7,20 @@
 # (e.g. locales/de/LC_MESSAGES/imio.annex.po)
 
 
+domain=imio.annex
+
 i18ndude rebuild-pot --pot $domain.pot --create $domain ../
 
-domain=imio.annex
-i18ndude sync --pot $domain.pot */LC_MESSAGES/$domain.po
-domain=plone
-i18ndude sync --pot $domain.pot */LC_MESSAGES/$domain.po
+declare -a languages=("fr")
+for lang in "${languages[@]}"; do
+		mkdir -p $lang/LC_MESSAGES
+done
+
+for lang in $(find . -mindepth 1 -maxdepth 1 -type d); do
+		if test -d $lang/LC_MESSAGES; then
+				touch $lang/LC_MESSAGES/$domain.po
+				touch $lang/LC_MESSAGES/plone.po
+				i18ndude sync --pot $domain.pot $lang/LC_MESSAGES/$domain.po
+				i18ndude sync --pot plone.pot $lang/LC_MESSAGES/plone.po
+		fi
+done
