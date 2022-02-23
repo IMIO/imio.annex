a18ff5867831fcbb81e8930812cfad84bcd18a40
Gauthier Bastien <gauthier.bastien@imio.be>
2022-02-23 10:24:00 +0100

    Fixed annex_content_created and annex_content_updated event handlers that receive now 2 parameters (obj and event) instead one single (event) now that we register the events for IAnnex

--- src/imio/annex/content/events.py
+++ src/imio/annex/content/events.py
@@ -18,17 +18,11 @@ from imio.annex.events import AnnexFileChangedEvent
 from imio.annex.content.annex import IAnnex
 
 
-def annex_content_created(event):
-    obj = event.object
-    if not IAnnex.providedBy(obj):
-        return
+def annex_content_created(obj, event):
     notify(AnnexFileChangedEvent(obj, obj.file))
 
 
-def annex_content_updated(event):
-    obj = event.object
-    if not IAnnex.providedBy(obj):
-        return
+def annex_content_updated(obj, event):
     if obj.file._blob._p_blob_uncommitted is not None:
         notify(AnnexFileChangedEvent(obj, obj.file))
 
