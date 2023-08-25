71256736865cf726789db39a9aa8c1148c905eeb
Gauthier Bastien <gauthier.bastien@imio.be>
2023-08-25 16:49:10 +0200

    Fixed conversion status update that was not working when using async conversion with `collective.documentviewer`. Display a action the `View preview` when a preview is available. See #PM-4064

--- src/imio/annex/patch.py
+++ src/imio/annex/patch.py
@@ -7,6 +7,7 @@ Created by mpeeters
 :license: GPL, see LICENCE.txt for more details.
 """
 
+from collective.documentviewer.async import JobRunner
 from collective.documentviewer.convert import Converter
 from imio.annex.events import ConversionStartedEvent
 from zope.event import notify
@@ -15,3 +16,8 @@ from zope.event import notify
 def converter_call(self, *args, **kwargs):
     notify(ConversionStartedEvent(self.context))
     return Converter._old___call__(self, *args, **kwargs)
+
+
+def jobrunner_queue_it(self):
+    JobRunner._old_queue_it(self)
+    notify(ConversionStartedEvent(self.object))
